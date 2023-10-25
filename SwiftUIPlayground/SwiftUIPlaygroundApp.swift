042b24d9f8d6fd9b3fde70e596c6aef87bcefb2d
Omar Alshammari <omar.alshammary@hotmail.com>
2023-10-25 23:00:36 +0300

    Initial Commit

---
+++ SwiftUIPlayground/SwiftUIPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPlaygroundApp.swift
+//  SwiftUIPlayground
+//
+//  Created by Omar Alshammari on 25/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
